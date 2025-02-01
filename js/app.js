02158f42a0f5b0d72b27bc7c89331dcd68c297e9
BkainP <bpauls@student.alamo.edu>
2025-01-31 23:31:54 -0600

    variables

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
+// Original console log
 console.log("Hello Professor");
+
+// Global variable
+var globalVar = 10;
+
+console.log("Global Variable:", globalVar);
+
+function exampleFunction() {
+    // Block variable
+    let blockVar = 5;
+
+    console.log("Block Variable:", blockVar);
+
+    // Mathematical operations
+    console.log("Sum:", globalVar + blockVar);
+    console.log("Product:", globalVar * blockVar);
+}
+
+exampleFunction();
+
+// String variable
+let stringVar = "Hello, World!";
+console.log("String Variable:", stringVar);
+
+// Object variable
+const objectVar = {
+    name: "Brad",
+    age: 28,
+    city: "Texas"
+};
+
+console.log("Object Variable:", objectVar);
